317ae1fec7c0df37dbe494792bfa195fd2634394
vilibert <vilibert@student.s19.be>
2024-06-11 19:47:15 +0200

    default http error, Print fix

--- Print.cpp
+++ Print.cpp
@@ -6,7 +6,7 @@
 /*   By: vilibert <vilibert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:20:53 by vilibert          #+#    #+#             */
-/*   Updated: 2024/06/11 17:02:48 by vilibert         ###   ########.fr       */
+/*   Updated: 2024/06/11 19:01:54 by vilibert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,52 @@ static std::tm *getTime(void)
 
 }
 
-void Print::print(std::string const &str)
+void Print::print(std::string status, std::string const &str)
 {
     std::tm *time = getTime();
-    std::cout << '[' << time->tm_year + 1900 << '-' << time->tm_mon + 1 << '-' << time->tm_mday << "][" << time->tm_hour << ':' << time->tm_min << ':' << time->tm_sec << "]    " << str << '\n';
+    std::cout << '[' << time->tm_year + 1900 << '-';
+    if (time->tm_mon + 1 < 10)
+        std::cout << '0';
+    std::cout << time->tm_mon + 1 << '-';
+    if (time->tm_mday < 10)
+        std::cout << '0';
+    std::cout << time->tm_mday << "][";
+    if (time->tm_hour < 10)
+        std::cout << '0';
+    std::cout << time->tm_hour << ':';
+    if (time->tm_min < 10)
+        std::cout << '0';   
+    std::cout << time->tm_min << ':';
+    if (time->tm_sec < 10)
+        std::cout << '0';   
+    std::cout << time->tm_sec << "]    " << status << "    " << str << '\n';
 }
 
-void Print::success_print(std::string const &str)
+void Print::success_print(std::string status, std::string const &str)
 {
-    std::tm *time = getTime();
-    std::cout << "\e[32m[" << time->tm_year + 1900 << '-' << time->tm_mon + 1 << '-' << time->tm_mday << "][" << time->tm_hour << ':' << time->tm_min << ':' << time->tm_sec << "]    " << str << "\e[30m\n";
+    std::cout << "\e[32m";
+    print(status, str);
+    std::cout << "\e[97m";
 }
 
-void Print::error_print(std::string const &str)
+void Print::error_print(std::string status, std::string const &str)
 {
     std::tm *time = getTime();
-    std::cerr << "\e[31m[" << time->tm_year + 1900 << '-' << time->tm_mon + 1 << '-' << time->tm_mday << "][" << time->tm_hour << ':' << time->tm_min << ':' << time->tm_sec << "]    " << str << "\e[30m\n";
+    std::cerr << "\e[31m[" << time->tm_year + 1900 << '-';
+    if (time->tm_mon + 1 < 10)
+        std::cerr << '0';
+    std::cerr << time->tm_mon + 1 << '-';
+    if (time->tm_mday < 10)
+        std::cerr << '0';
+    std::cerr << time->tm_mday << "][";
+    if (time->tm_hour < 10)
+        std::cerr << '0';
+    std::cerr << time->tm_hour << ':';
+    if (time->tm_min < 10)
+        std::cerr << '0';   
+    std::cerr << time->tm_min << ':';
+    if (time->tm_sec < 10)
+        std::cerr << '0';   
+    std::cerr << time->tm_sec << "]    " << status << "    " << str << "\e[97m\n";
 
 }
--- Print.hpp
+++ Print.hpp
@@ -6,7 +6,7 @@
 /*   By: vilibert <vilibert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:20:47 by vilibert          #+#    #+#             */
-/*   Updated: 2024/06/11 17:01:45 by vilibert         ###   ########.fr       */
+/*   Updated: 2024/06/11 17:59:59 by vilibert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,17 @@
 #include <iostream>
 #include <ctime>
 
+#define DEBUG "[DEBUG]"
+#define INFO "[INFO]"
+#define ERROR "[ERROR]"
+#define CRASH "[CRASH]"
+
 class Print
 {
     public:
-        static void print(std::string const &str);
-        static void success_print(std::string const &str);
-        static void error_print(std::string const &str);
+        static void print(std::string status, std::string const &str);
+        static void success_print(std::string status, std::string const &str);
+        static void error_print(std::string status, std::string const &str);
     private:
         Print();
         ~Print();
