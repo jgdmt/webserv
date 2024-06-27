c2814824f5e4b4b7d9aee729fefe7e375df390a1
vilibert <vilibert@student.s19.be>
2024-06-27 18:46:42 +0200

    reformating

--- socket/Response.cpp
+++ socket/Response.cpp
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   Response.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgoudema <jgoudema@student.s19.be>         +#+  +:+       +#+        */
+/*   By: vilibert <vilibert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 14:58:32 by vilibert          #+#    #+#             */
-/*   Updated: 2024/06/27 15:39:31 by jgoudema         ###   ########.fr       */
+/*   Updated: 2024/06/27 18:36:47 by vilibert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Response.hpp"
 #include "CGI.hpp"
 
-Response::Response(Request* req, Server* serv)
+Response::Response(Request *req, Server *serv)
 {
     _req = req;
     _serv = serv;
@@ -47,7 +47,7 @@ void Response::cut(int pos)
 void Response::genHeader(std::string type)
 {
     std::time_t now = std::time(NULL);
-    std::tm *time =  std::localtime(&now);
+    std::tm *time = std::localtime(&now);
     char buff[80];
 
     _buffer.clear();
@@ -62,15 +62,14 @@ void Response::genHeader(std::string type)
     _buffer.append("Server: IsWatchingYou\r\n");
 }
 
-
 void Response::genBody(std::string path)
 {
     std::ifstream FileDescriptor(path.c_str());
-    if(FileDescriptor.fail())
+    if (FileDescriptor.fail())
     {
         Print::print(ERROR, "Couldn't open body file");
         error("500", "Internal Server Error");
-        return ;
+        return;
     }
     std::ostringstream FileStream;
     FileStream << FileDescriptor.rdbuf();
@@ -79,7 +78,7 @@ void Response::genBody(std::string path)
     _buffer.append("Content-Length: " + to_string(buff.length()) + "\r\n\r\n");
     // std::cout << _buffer;
     // std::cout << path << '\n';
-    if(_req->getMethod() == "HEAD")
+    if (_req->getMethod() == "HEAD")
         return;
     _buffer.append(buff);
     // _buffer.append("\r\n");
@@ -92,22 +91,22 @@ void Response::genDirListing(std::string path)
     body.append("<html><head><title>Index of " + path + "</title></head>");
     body.append("<body><h1>Index of " + path + "</h1><hr><pre>");
     DIR *dir = opendir(path.c_str());
-    struct stat	path_stat;
+    struct stat path_stat;
     std::string relativeUrl;
-    if(!dir)
+    if (!dir)
     {
         Print::print(ERROR, "Couldn't open directory");
         error("500", "Internal Server Error");
-        return ;
+        return;
     }
-    struct dirent* file;
-    while((file = readdir(dir)) != NULL)
+    struct dirent *file;
+    while ((file = readdir(dir)) != NULL)
     {
         if ((std::string)file->d_name == ".")
             continue;
         stat((path + file->d_name).c_str(), &path_stat);
-            relativeUrl = file->d_name;
-        if(S_ISDIR(path_stat.st_mode))
+        relativeUrl = file->d_name;
+        if (S_ISDIR(path_stat.st_mode))
             relativeUrl.append("/");
         body.append("<a href=\"" + relativeUrl + "\">" + relativeUrl + "</a>\n");
     }
@@ -116,7 +115,6 @@ void Response::genDirListing(std::string path)
     _buffer.append("\r\n");
     _buffer.append(body);
 }
-
 
 void Response::error(std::string httpErrorCode, std::string httpErrorMessage)
 {
@@ -145,34 +143,33 @@ bool Response::checkCGI(std::string path, Route *route)
         return false;
     }
     else
-    return false;
+        return false;
 }
-
 
 void Response::check_path(std::string path, Route *route)
 {
-    struct stat	path_stat;
+    struct stat path_stat;
 
-	stat(path.c_str(), &path_stat);
-	if (!S_ISDIR(path_stat.st_mode))
+    stat(path.c_str(), &path_stat);
+    if (!S_ISDIR(path_stat.st_mode))
     {
         if (access(path.c_str(), F_OK))
             error("404", "Not Found");
-        else if (access(path.c_str(),  R_OK))
+        else if (access(path.c_str(), R_OK))
             error("403", "Forbidden");
         else
         {
-			if (checkCGI(path, route))
-				return ;
+            if (checkCGI(path, route))
+                return;
             unsigned int i = 0;
             std::string myme = MIME_TYPE(path.substr(path.find_last_of('.') + 1));
-            while(i < _req->getAcceptSize())
+            while (i < _req->getAcceptSize())
             {
-                if(myme == _req->getAccept(i) || "*/*" == _req->getAccept(i))
+                if (myme == _req->getAccept(i) || "*/*" == _req->getAccept(i))
                     break;
                 i++;
             }
-            if(_req->getAcceptSize() != 0 && i == _req->getAcceptSize())
+            if (_req->getAcceptSize() != 0 && i == _req->getAcceptSize())
                 error("406", "Not Acceptable");
             else
             {
@@ -183,70 +180,69 @@ void Response::check_path(std::string path, Route *route)
     }
     else
     {
-    if(path[path.size() - 1] != '/')
-    {
-        genHeader("301 Moved Permanently");
-        _buffer.append("Location: " + path + "/\r\n\r\n");
-        return;
-    }
-    std::cout << path << '\n';
-    if (route->getdefaultFileForDirectory().size() != 0 && route->getPath() + "/" == path)
-    {
-        if(route->getdefaultFileForDirectory()[0] == '/')
-            route->setdefaultFileForDirectory(route->getdefaultFileForDirectory().substr(1));
-        check_path(path + route->getdefaultFileForDirectory(), route);
-        return;
-    }
-    unsigned int bestMatchKey = _req->getAcceptSize();
-    std::string bestMatchValue;
-    DIR *dir = opendir(path.c_str());
-    if(!dir)
-    {
-        Print::print(ERROR, "Couldn't open directory");
-        error("500", "Internal Server Error");
-        return ;
-    }
-    struct dirent* file;
-    while((file = readdir(dir)) != NULL)
-    {
-        if (((std::string)file->d_name).find("index.") == 0)
+        if (path[path.size() - 1] != '/')
         {
-            stat((path + file->d_name).c_str(), &path_stat);
-            if(S_ISDIR(path_stat.st_mode))
-                continue;
-            std::string tmp = file->d_name;
-            std::string myme = MIME_TYPE(tmp.substr(tmp.find_last_of('.') + 1));
-            unsigned int i = 0;
-            while(i < _req->getAcceptSize())
-            {
-                if(myme == _req->getAccept(i) || "*/*" == _req->getAccept(i))
-                    break;
-                i++;
-            }
-            if (i != _req->getAcceptSize() && i < bestMatchKey)
-            {
-                bestMatchKey = i;
-                bestMatchValue = tmp;
-            }
-
+            std::cout << path << std::endl;
+            genHeader("301 Moved Permanently");
+            _buffer.append("Location: " + path + "/\r\n\r\n");
+            return;
         }
-    }
-    if(bestMatchValue.size())
-    {
-        if (path[path.size() -1] != '/')
-            path.append("/");
-        genHeader("200 OK");
-        genBody(path + bestMatchValue); 
-    }
-    else if ((route && route->IsListing()))
-    {
-        genHeader("200 OK");
-        genDirListing(path);
-    }
-    else
-    {
-        error("403", "Forbidden");
-    }
+        if (route->getdefaultFileForDirectory().size() != 0 && route->getPath() + "/" == path)
+        {
+            if (route->getdefaultFileForDirectory()[0] == '/')
+                route->setdefaultFileForDirectory(route->getdefaultFileForDirectory().substr(1));
+            check_path(path + route->getdefaultFileForDirectory(), route);
+            return;
+        }
+        unsigned int bestMatchKey = _req->getAcceptSize();
+        std::string bestMatchValue;
+        DIR *dir = opendir(path.c_str());
+        if (!dir)
+        {
+            Print::print(ERROR, "Couldn't open directory");
+            error("500", "Internal Server Error");
+            return;
+        }
+        struct dirent *file;
+        while ((file = readdir(dir)) != NULL)
+        {
+            if (((std::string)file->d_name).find("index.") == 0)
+            {
+                stat((path + file->d_name).c_str(), &path_stat);
+                if (S_ISDIR(path_stat.st_mode))
+                    continue;
+                std::string tmp = file->d_name;
+                std::string myme = MIME_TYPE(tmp.substr(tmp.find_last_of('.') + 1));
+                unsigned int i = 0;
+                while (i < _req->getAcceptSize())
+                {
+                    if (myme == _req->getAccept(i) || "*/*" == _req->getAccept(i))
+                        break;
+                    i++;
+                }
+                if (i != _req->getAcceptSize() && i < bestMatchKey)
+                {
+                    bestMatchKey = i;
+                    bestMatchValue = tmp;
+                }
+            }
+        }
+        if (bestMatchValue.size())
+        {
+            if (path[path.size() - 1] != '/')
+                path.append("/");
+            genHeader("200 OK");
+            genBody(path + bestMatchValue);
+        }
+        else if ((route && route->IsListing()))
+        {
+            genHeader("200 OK");
+            genDirListing(path);
+        }
+        else
+        {
+            error("403", "Forbidden");
+        }
     }
 }
 
@@ -259,12 +255,12 @@ void Response::init(void)
     std::string chunkUri = _req->getUri().substr(pos, next - pos);
     Route *route = NULL;
 
-    for(unsigned int i = 0; i < _serv->getRoutesNumber(); i++)
+    for (unsigned int i = 0; i < _serv->getRoutesNumber(); i++)
     {
-        if(_serv->getRoute(i)->getRedirection() == chunkUri)
+        if (_serv->getRoute(i)->getRedirection() == chunkUri)
         {
             route = _serv->getRoute(i);
-            break ;
+            break;
         }
     }
     while (route != NULL)
@@ -281,16 +277,19 @@ void Response::init(void)
             if (route->getRoute(i)->getRedirection() == chunkUri)
             {
                 route = route->getRoute(i);
-                break ;
+                break;
             }
-			i++;
+            i++;
         }
         if (i == nbRoute)
-            break ;
+            break;
     }
     std::string path;
-    if(route == NULL)
-        path = _req->getUri();
+    if (route == NULL)
+    {
+        error("404", "Not Found");
+        return;
+    }
     else
     {
         if (route && !route->isAutorizedMethod(_req->getMethod()))
@@ -298,12 +297,13 @@ void Response::init(void)
             error("405", "Method Not Allowed");
             return;
         }
-        if(route->getPath()[route->getPath().size() - 1] == '/')
-            route->setPath(route->getPath().substr(0, route->getPath().size() - 1)); 
+        if (route->getPath()[route->getPath().size() - 1] == '/')
+            route->setPath(route->getPath().substr(0, route->getPath().size() - 1));
         path = route->getPath();
-        if(pos < _req->getUri().size())
+        if (pos < _req->getUri().size())
             path.append(_req->getUri().substr(pos, _req->getUri().size() - pos));
     }
+    std::cout << path << std::endl;
     check_path(path, route);
-    return ;
+    return;
 }
